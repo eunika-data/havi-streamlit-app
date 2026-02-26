aec589f01fc46318d9db4afb375b8c157926afbe
Eunika Niedzwiecka <eunikaniedzwiecka@gmail.com>
2026-02-26 05:04:36 +0100

    add simple password protection

--- happ.py
+++ happ.py
@@ -8,6 +8,25 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == "havi34":
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Hasło", type="password", on_change=password_entered, key="password")
+        st.stop()
+    elif not st.session_state["password_correct"]:
+        st.text_input("Hasło", type="password", on_change=password_entered, key="password")
+        st.error("Nieprawidłowe hasło")
+        st.stop()
+
+check_password()
+
 # =========================
 # 0) PAGE + THEME
 # =========================
@@ -16,6 +35,8 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
 
 st.markdown(
     """
